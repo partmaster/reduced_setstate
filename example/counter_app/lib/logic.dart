11a03b4358713122cb802ddc4b63ac94288dab85
Steffen Nowacki <steffen.nowacki@partmaster.de>
2023-03-15 08:26:53 +0100

    update reduced dependency

--- example/counter_app/lib/logic.dart
+++ example/counter_app/lib/logic.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:reduced/reduced.dart';
+import 'package:reduced/callbacks.dart';
 
 class CounterIncremented extends Event<int> {
   @override
