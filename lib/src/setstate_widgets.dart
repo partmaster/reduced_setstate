dc3e6d0cba521d2edebf49c6aea496aacbb4ae7d
Steffen Nowacki <steffen.nowacki@partmaster.de>
2023-04-07 08:19:19 +0200

    add RouteNameProps

--- lib/src/setstate_widgets.dart
+++ lib/src/setstate_widgets.dart
@@ -6,22 +6,31 @@ import 'package:reduced/reduced.dart';
 import 'inherited_widgets.dart';
 import 'setstate_store.dart';
 
+abstract class RouteNameProps<T> {
+  T copyWithRouteName(String? routeName);
+}
+
 class ReducedConsumer<S, P> extends StatelessWidget {
   const ReducedConsumer({
     super.key,
     required this.builder,
     required this.mapper,
+    this.routeName,
   });
 
   final WidgetFromPropsBuilder<P> builder;
   final StateToPropsMapper<S, P> mapper;
+  final String? routeName;
 
   @override
   Widget build(BuildContext context) =>
       _build(InheritedValueWidget.of<ReducedStoreAndState<S>>(context).store);
 
   Widget _build(Store<S> store) => InheritedValueWidget(
-        value: mapper(store.state, store),
+        value: _copyWithRouteName(mapper(store.state, store)),
         child: ReducedStatefulBuilderWidget<P>(builder: builder),
       );
+
+  P _copyWithRouteName(P props) =>
+      props is RouteNameProps<P> ? props.copyWithRouteName(routeName) : props;
 }
